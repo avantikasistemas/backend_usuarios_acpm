9eb245cae433ff4d07a69115fc89703de599be31
Victor Nieto <sistemas@avantika.com.co>
2026-04-16 09:24:30 -0500

    se agrega url al cors

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ app.version = "0.0.1"
 app.add_middleware(JSONMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permitir todos los orígenes; para producción, especifica los orígenes permitidos.
+    allow_origins=["https://usuarios-acpm.avantika.com.co"],
+    # allow_origins=["*"],  # Permitir todos los orígenes; para producción, especifica los orígenes permitidos.
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos; puedes especificar los métodos permitidos.
     allow_headers=["*"],  # Permitir todos los encabezados; puedes especificar los encabezados permitidos.
@@ -29,5 +30,5 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         port=8018,
-        reload=True,  # Solo para desarrollo; no usar en producción
+        # reload=True,  # Solo para desarrollo; no usar en producción
     )
